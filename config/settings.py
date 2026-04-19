4b648fb9638f970fc35285c0c04e5e97a7b929d8
Marto <martinkimani42@gmail.com>
2026-04-19 23:19:37 +0300

    Add CSRF_TRUSTED_ORIGINS for production deployment

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,12 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost:8000',
+    cast=Csv()
+)
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
