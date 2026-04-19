1239f14a9d94975b20869ced8af886cbb552b525
Marto <martinkimani42@gmail.com>
2026-04-19 18:27:42 +0300

    Fix settings: add templates dir, LOGIN_URL and production config

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('users.urls', namespace='users')),
-    path('',include('fields.urls',namespace='fields')),
-
+    path('accounts/', include('users.urls', namespace='users')),
+    path('', include('fields.urls', namespace='fields')),
 ]
